9abf6b9c5ad9bd2cb2ddfb94bf8616e3d4ff2d23
ShAd0w1911 <unbundleracers@gmail.com>
2020-04-19 22:46:00 +0500

    changes in projectTab

--- src/Projects.js
+++ src/Projects.js
@@ -1,5 +1,157 @@
 import React from 'react'
+import { MDBRow, MDBCol, MDBCard, MDBBtn, MDBIcon, MDBCardTitle } from 'mdbreact'; 
 
 export const Projects = () => (
-    <p>Hello PROJECTS NO WHERE TO BE FOUND FUCK THIS SHITE!!!!</p>
+    <div>
+        <MDBRow className="mx-1" style={{marginTop:"20px"}}>
+          <MDBCol md="4" className="mb-4 mb-md-0">
+            <section>
+              <MDBCard style={{ backgroundImage: "url('https://mdbootstrap.com/img/Photos/Horizontal/City/6-col/img%20(47).jpg')" }}>
+                <div className="text-white text-center d-flex align-items-center img-gradient-overlay py-5 px-4 transparentBg">
+                  <div className="transparentBg">
+                    <h5 className="orange-text"><MDBIcon icon="desktop" /> Software</h5>
+                    <MDBCardTitle tag="h3" className="pt-2"><strong>This is card title</strong></MDBCardTitle>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat fugiat, laboriosam, voluptatem,
+                        optio vero odio nam sit officia accusamus minus error nisi architecto nulla ipsum dignissimos.
+                    Odit sed qui, dolorum!.</p>
+                    <MDBBtn style={{backgroundColor:"#fb7840", color:"#fff"}}><MDBIcon icon="clone left" /> View project</MDBBtn>
+                  </div>
+                </div>
+              </MDBCard>
+            </section>
+          </MDBCol>
+          <MDBCol md="4" className="mb-4 mb-md-0">
+            <section>
+              <MDBCard style={{ backgroundImage: "url('https://mdbootstrap.com/img/Photos/Horizontal/City/6-col/img%20(47).jpg')" }}>
+                <div className="text-white text-center d-flex align-items-center img-gradient-overlay py-5 px-4">
+                  <div  className="transparentBg">
+                    <h5 className="orange-text"><MDBIcon icon="desktop" /> Software</h5>
+                    <MDBCardTitle tag="h3" className="pt-2"><strong>This is card title</strong></MDBCardTitle>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat fugiat, laboriosam, voluptatem,
+                        optio vero odio nam sit officia accusamus minus error nisi architecto nulla ipsum dignissimos.
+                    Odit sed qui, dolorum!.</p>
+                    <MDBBtn style={{backgroundColor:"#fb7840", color:"#fff"}}><MDBIcon icon="clone left" /> View project</MDBBtn>
+                  </div>
+                </div>
+              </MDBCard>
+            </section>
+          </MDBCol>
+          <MDBCol md="4" className="mb-4 mb-md-0">
+            <section>
+              <MDBCard style={{ backgroundImage: "url('https://mdbootstrap.com/img/Photos/Horizontal/City/6-col/img%20(47).jpg')" }}>
+                <div className="text-white text-center d-flex align-items-center img-gradient-overlay py-5 px-4">
+                  <div  className="transparentBg">
+                    <h5 className="orange-text"><MDBIcon icon="desktop" /> Software</h5>
+                    <MDBCardTitle tag="h3" className="pt-2"><strong>This is card title</strong></MDBCardTitle>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat fugiat, laboriosam, voluptatem,
+                        optio vero odio nam sit officia accusamus minus error nisi architecto nulla ipsum dignissimos.
+                    Odit sed qui, dolorum!.</p>
+                    <MDBBtn style={{backgroundColor:"#fb7840", color:"#fff"}}><MDBIcon icon="clone left" /> View project</MDBBtn>
+                  </div>
+                </div>
+              </MDBCard>
+            </section>
+          </MDBCol>
+        </MDBRow>
+        <MDBRow className="mx-1" style={{marginTop:"20px"}}>
+          <MDBCol md="4" className="mb-4 mb-md-0">
+            <section>
+              <MDBCard style={{ backgroundImage: "url('https://mdbootstrap.com/img/Photos/Horizontal/City/6-col/img%20(47).jpg')" }}>
+                <div className="text-white text-center d-flex align-items-center img-gradient-overlay py-5 px-4 transparentBg">
+                  <div className="transparentBg">
+                    <h5 className="orange-text"><MDBIcon icon="desktop" /> Software</h5>
+                    <MDBCardTitle tag="h3" className="pt-2"><strong>This is card title</strong></MDBCardTitle>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat fugiat, laboriosam, voluptatem,
+                        optio vero odio nam sit officia accusamus minus error nisi architecto nulla ipsum dignissimos.
+                    Odit sed qui, dolorum!.</p>
+                    <MDBBtn style={{backgroundColor:"#fb7840", color:"#fff"}}><MDBIcon icon="clone left" /> View project</MDBBtn>
+                  </div>
+                </div>
+              </MDBCard>
+            </section>
+          </MDBCol>
+          <MDBCol md="4" className="mb-4 mb-md-0">
+            <section>
+              <MDBCard style={{ backgroundImage: "url('https://mdbootstrap.com/img/Photos/Horizontal/City/6-col/img%20(47).jpg')" }}>
+                <div className="text-white text-center d-flex align-items-center img-gradient-overlay py-5 px-4">
+                  <div  className="transparentBg">
+                    <h5 className="orange-text"><MDBIcon icon="desktop" /> Software</h5>
+                    <MDBCardTitle tag="h3" className="pt-2"><strong>This is card title</strong></MDBCardTitle>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat fugiat, laboriosam, voluptatem,
+                        optio vero odio nam sit officia accusamus minus error nisi architecto nulla ipsum dignissimos.
+                    Odit sed qui, dolorum!.</p>
+                    <MDBBtn style={{backgroundColor:"#fb7840", color:"#fff"}}><MDBIcon icon="clone left" /> View project</MDBBtn>
+                  </div>
+                </div>
+              </MDBCard>
+            </section>
+          </MDBCol>
+          <MDBCol md="4" className="mb-4 mb-md-0">
+            <section>
+              <MDBCard style={{ backgroundImage: "url('https://mdbootstrap.com/img/Photos/Horizontal/City/6-col/img%20(47).jpg')" }}>
+                <div className="text-white text-center d-flex align-items-center img-gradient-overlay py-5 px-4">
+                  <div  className="transparentBg">
+                    <h5 className="orange-text"><MDBIcon icon="desktop" /> Software</h5>
+                    <MDBCardTitle tag="h3" className="pt-2"><strong>This is card title</strong></MDBCardTitle>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat fugiat, laboriosam, voluptatem,
+                        optio vero odio nam sit officia accusamus minus error nisi architecto nulla ipsum dignissimos.
+                    Odit sed qui, dolorum!.</p>
+                    <MDBBtn style={{backgroundColor:"#fb7840", color:"#fff"}}><MDBIcon icon="clone left" /> View project</MDBBtn>
+                  </div>
+                </div>
+              </MDBCard>
+            </section>
+          </MDBCol>
+        </MDBRow>
+        <MDBRow className="mx-1" style={{marginTop:"20px"}}>
+          <MDBCol md="4" className="mb-4 mb-md-0">
+            <section>
+              <MDBCard style={{ backgroundImage: "url('https://mdbootstrap.com/img/Photos/Horizontal/City/6-col/img%20(47).jpg')" }}>
+                <div className="text-white text-center d-flex align-items-center img-gradient-overlay py-5 px-4 transparentBg">
+                  <div className="transparentBg">
+                    <h5 className="orange-text"><MDBIcon icon="desktop" /> Software</h5>
+                    <MDBCardTitle tag="h3" className="pt-2"><strong>This is card title</strong></MDBCardTitle>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat fugiat, laboriosam, voluptatem,
+                        optio vero odio nam sit officia accusamus minus error nisi architecto nulla ipsum dignissimos.
+                    Odit sed qui, dolorum!.</p>
+                    <MDBBtn style={{backgroundColor:"#fb7840", color:"#fff"}}><MDBIcon icon="clone left" /> View project</MDBBtn>
+                  </div>
+                </div>
+              </MDBCard>
+            </section>
+          </MDBCol>
+          <MDBCol md="4" className="mb-4 mb-md-0">
+            <section>
+              <MDBCard style={{ backgroundImage: "url('https://mdbootstrap.com/img/Photos/Horizontal/City/6-col/img%20(47).jpg')" }}>
+                <div className="text-white text-center d-flex align-items-center img-gradient-overlay py-5 px-4">
+                  <div  className="transparentBg">
+                    <h5 className="orange-text"><MDBIcon icon="desktop" /> Software</h5>
+                    <MDBCardTitle tag="h3" className="pt-2"><strong>This is card title</strong></MDBCardTitle>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat fugiat, laboriosam, voluptatem,
+                        optio vero odio nam sit officia accusamus minus error nisi architecto nulla ipsum dignissimos.
+                    Odit sed qui, dolorum!.</p>
+                    <MDBBtn style={{backgroundColor:"#fb7840", color:"#fff"}}><MDBIcon icon="clone left" /> View project</MDBBtn>
+                  </div>
+                </div>
+              </MDBCard>
+            </section>
+          </MDBCol>
+          <MDBCol md="4" className="mb-4 mb-md-0">
+            <section>
+              <MDBCard style={{ backgroundImage: "url('https://mdbootstrap.com/img/Photos/Horizontal/City/6-col/img%20(47).jpg')" }}>
+                <div className="text-white text-center d-flex align-items-center img-gradient-overlay py-5 px-4">
+                  <div  className="transparentBg">
+                    <h5 className="orange-text"><MDBIcon icon="desktop" /> Software</h5>
+                    <MDBCardTitle tag="h3" className="pt-2"><strong>This is card title</strong></MDBCardTitle>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat fugiat, laboriosam, voluptatem,
+                        optio vero odio nam sit officia accusamus minus error nisi architecto nulla ipsum dignissimos.
+                    Odit sed qui, dolorum!.</p>
+                    <MDBBtn style={{backgroundColor:"#fb7840", color:"#fff"}}><MDBIcon icon="clone left" /> View project</MDBBtn>
+                  </div>
+                </div>
+              </MDBCard>
+            </section>
+          </MDBCol>
+        </MDBRow>
+    </div>
 )
