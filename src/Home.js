2c4121d0cf315a12650917052bcd56dae82d3b50
ShAd0w1911 <unbundleracers@gmail.com>
2020-04-21 03:23:13 +0500

    added media query for contact

--- src/Home.js
+++ src/Home.js
@@ -58,14 +58,16 @@ export const Home = () => (
                             <h1 style={{textAlign:"center"}}>Nine dedicated developers that strive for <span style={{color:"#fb7840"}}>perfection</span>.</h1>
                         </Col>
                         <Col lg md>
-                            <p style={{textAlign:"center", fontWeight:"100px"}}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+                            <p style={{textAlign:"center", fontWeight:"100px"}}>
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
+                            </p>
                         </Col>
                     </Row>
                 </div>
                 <br/>
-                <div className="img-gradient">
-                    <img src="https://wallpaperplay.com/walls/full/3/a/e/77868.jpg" height="700px" className="img-fluid"/>
-                </div>
                 <Container id="aboutCont">
                     <div>
                         <Row>
@@ -78,6 +80,9 @@ export const Home = () => (
                         </Row>
                     </div>
                 </Container>
+                <div className="img-gradient">
+                    <img src="https://wallpaperplay.com/walls/full/3/a/e/77868.jpg" height="700px" className="img-fluid"/>
+                </div>
             </Container>
         </div>
     </div>
--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { Router as Router } from 'react-router-dom';
+//import { Router as Router } from 'react-router-dom';
 
-ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 
 // If you want your app to work offline and load faster, you can change
