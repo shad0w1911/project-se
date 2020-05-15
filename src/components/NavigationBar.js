cd2e56f683dd3232f3974aefd401b6a40657a673
ShAd0w1911 <unbundleracers@gmail.com>
2020-05-16 02:24:45 +0500

    major fixes

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -22,16 +22,16 @@ export default function NavigationBar() {
     return (
         <Styles>
             <Navbar  variant="dark" expand="lg" sticky="top"  style={{backgroundColor: "#000", color: "#fff", borderBottom: "1px solid #fb7840"}}>
-                <Navbar.Brand href="/" style={{ fontSize: "24px", fontWeight: "10em" }}>SARWAR ENTERPRISES</Navbar.Brand>
+                <Navbar.Brand href="https://shad0w1911.github.io/project-se/#/" style={{ fontSize: "24px", fontWeight: "10em" }}>SARWAR ENTERPRISES</Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" className="toggler"/>
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="ml-auto">
-                    <Nav.Item><Nav.Link href="/">HOME</Nav.Link></Nav.Item>
-                    <Nav.Item><Nav.Link href="/About">ABOUT</Nav.Link></Nav.Item>
-                    <Nav.Item><Nav.Link href="/Services">SERVICES</Nav.Link></Nav.Item>
-                    <Nav.Item><Nav.Link href="/Projects">PROJECTS</Nav.Link></Nav.Item>
-                    <Nav.Item><Nav.Link href="/Partners">PARTNERS</Nav.Link></Nav.Item>
-                    <Nav.Item><Nav.Link href="/Contact">CONTACT</Nav.Link></Nav.Item>
+                    <Nav.Item><Nav.Link href="https://shad0w1911.github.io/project-se/#/">HOME</Nav.Link></Nav.Item>
+                    <Nav.Item><Nav.Link href="https://shad0w1911.github.io/project-se/#/About">ABOUT</Nav.Link></Nav.Item>
+                    <Nav.Item><Nav.Link href="https://shad0w1911.github.io/project-se/#/Services">SERVICES</Nav.Link></Nav.Item>
+                    <Nav.Item><Nav.Link href="https://shad0w1911.github.io/project-se/#/Projects">PROJECTS</Nav.Link></Nav.Item>
+                    <Nav.Item><Nav.Link href="https://shad0w1911.github.io/project-se/#/Partners">PARTNERS</Nav.Link></Nav.Item>
+                    <Nav.Item><Nav.Link href="https://shad0w1911.github.io/project-se/#/Contact">CONTACT</Nav.Link></Nav.Item>
                     </Nav>
                 </Navbar.Collapse>
                 </Navbar>
