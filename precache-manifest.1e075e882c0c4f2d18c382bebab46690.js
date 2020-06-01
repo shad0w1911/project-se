26a2705507d3615e2fc092486fc3382a374f3a08
ShAd0w1911 <unbundleracers@gmail.com>
2020-06-01 19:03:53 +0500

    Updates

---
+++ precache-manifest.1e075e882c0c4f2d18c382bebab46690.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e102050d09b13db55c0cab47c79d4ad",
+    "url": "/project-se/index.html"
+  },
+  {
+    "revision": "2b1776ebaf9451ab328d",
+    "url": "/project-se/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "d644cf3325a1cd1c81f6",
+    "url": "/project-se/static/css/main.ba4d4440.chunk.css"
+  },
+  {
+    "revision": "2b1776ebaf9451ab328d",
+    "url": "/project-se/static/js/2.feba97e6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/project-se/static/js/2.feba97e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d644cf3325a1cd1c81f6",
+    "url": "/project-se/static/js/main.5087c739.chunk.js"
+  },
+  {
+    "revision": "70c79d1b07d81109b015",
+    "url": "/project-se/static/js/runtime-main.0f0a5856.js"
+  },
+  {
+    "revision": "b5dca10a98677c220b79362f7962cba1",
+    "url": "/project-se/static/media/DHA-City-Karachi-Main-Gate.b5dca10a.jpg"
+  },
+  {
+    "revision": "837c72c979626ff5a884dc3215bd432d",
+    "url": "/project-se/static/media/SE-logo.837c72c9.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/project-se/precache-manifest.c7b154dc04bdc82dd99090b1d0dd0d7d.js"
+  "/project-se/precache-manifest.1e075e882c0c4f2d18c382bebab46690.js"
 );
 
 self.addEventListener('message', (event) => {
