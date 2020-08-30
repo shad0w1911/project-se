c3955a5e8696ba7ca9bddd588f9dad115c48e279
Hassan Uddin Sheikh <unbundleracers@gmail.com>
2020-08-30 18:03:19 +0500

    few design changes and updated project tab

--- src/Projects.js
+++ src/Projects.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Row, Col, Container } from "react-bootstrap";
 import './App.css';
+import dhaOasis from './assets/dhaOasis.jpg';
 
 export const Projects = () => (
     <div style={{height:"100%"}}>
@@ -31,39 +32,59 @@ export const Projects = () => (
                         </Row>
                 </div>
                 <div>
-                    <p style={{fontSize:"50px", fontWeight:"900", textAlign:"center", paddingBottom:"50px", color:"#fb7820"}}>Our Projects</p>
+                    <p style={{fontSize:"50px", fontWeight:"900", textAlign:"center", paddingBottom:"50px", color:"#fb7820"}}>Current Projects</p>
                 </div>
                 
                 <section>
                     <Row>
                         <Col lg md>
-                            <div style={{width:"350px"}}  className="projectDisplayContainer">
+                            <div style={{width:"350px"}}>
                                 <img alt="nationalStadium" src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/cc/Nat_Std01.JPG/1200px-Nat_Std01.JPG" className="image"/>
-                                <div>
+                                <div style={{backgroundColor: "#141414"}}>
                                     <h3 style={{textAlign:"center"}}>National Stadium</h3>
                                     <h5 style={{textAlign:"center"}}>Our Services</h5>
-                                    <p style={{textAlign:"justify"}}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis, officiis. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis, officiis.</p>
+                                    <p style={{marginLeft:"10px"}}>
+                                        <i class="fa fa-chevron-right" aria-hidden="true" style={{color:"#fb7820"}}></i> Plumbing & Sanitary Work <br/>
+                                        <i class="fa fa-chevron-right" aria-hidden="true" style={{color:"#fb7820"}}></i> Fire-fighting System <br/>
+                                        <i class="fa fa-chevron-right" aria-hidden="true" style={{color:"#fb7820"}}></i> Booster Centrifugal Pump <br/>
+                                        <i class="fa fa-chevron-right" aria-hidden="true" style={{color:"#fb7820"}}></i> External Rain Water <br/>
+                                         
+                                    </p>
                                 </div>
                             </div>
                         </Col>
                         <Col lg md>
-                            <img alt="nationalStadium" src="https://tribune-reloaded.s3.amazonaws.com/media/images/1057350-image-1456854258/1057350-image-1456854258.JPG" className="image" />
-                            <div>
-                                <h3 style={{textAlign:"center"}}>BHP Billiton Pakistan</h3>
+                            <img alt="crescentSteel" src="https://crescent.com.pk/wp-content/gallery/steel_1/184_8496.jpg" className="image" />
+                            <div style={{backgroundColor: "#141414"}}>
+                                <h3 style={{textAlign:"center"}}>Crescent Steel Hostel</h3>
                                 <h5 style={{textAlign:"center"}}>Our Services</h5>
-                                <p style={{textAlign:"justify"}}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis, officiis. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis, officiis.</p>
+                                <p style={{marginLeft:"10px"}}>
+                                    <i class="fa fa-chevron-right" aria-hidden="true" style={{color:"#fb7820"}}></i> Plumbing & Sanitary Work <br/>
+                                    <i class="fa fa-chevron-right" aria-hidden="true" style={{color:"#fb7820"}}></i> Fire-fighting System <br/>
+                                    <i class="fa fa-chevron-right" aria-hidden="true" style={{color:"#fb7820"}}></i> Booster Centrifugal Pump <br/>
+                                    <i class="fa fa-chevron-right" aria-hidden="true" style={{color:"#fb7820"}}></i> External Rain Water <br/>
+                                     
+                                </p>
                             </div>
                             </Col>
                         <Col lg md>
-                            <img alt="nationalStadium" src="https://www.samaa.tv/wp-content/uploads/2018/05/bahria.jpg" className="image" />
-                            <div>
-                                <h3 style={{textAlign:"center"}}>Bahria Town Karachi</h3>
+                            <img alt="nationalStadium" src={dhaOasis} className="image" />
+                            <div style={{backgroundColor: "#141414"}}>
+                                <h3 style={{textAlign:"center"}}>DHA Oasis Karachi</h3>
                                 <h5 style={{textAlign:"center"}}>Our Services</h5>
-                                <p style={{textAlign:"justify"}}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis, officiis. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis, officiis.</p>
+                                <p style={{marginLeft:"10px"}}>
+                                    <i class="fa fa-chevron-right" aria-hidden="true" style={{color:"#fb7820"}}></i> Plumbing & Sanitary Work <br/>
+                                    <i class="fa fa-chevron-right" aria-hidden="true" style={{color:"#fb7820"}}></i> Fire-fighting System <br/>
+                                    <i class="fa fa-chevron-right" aria-hidden="true" style={{color:"#fb7820"}}></i> Booster Centrifugal Pump <br/>
+                                    <i class="fa fa-chevron-right" aria-hidden="true" style={{color:"#fb7820"}}></i> External Rain Water System <br/>
+                                     
+                                </p>
                             </div>
                         </Col>
                     </Row>
-                    
+                    <br/>
+                    <p><hr style={{color:"#fb7820", border:"1px solid #fb7820"}}/></p>
+                    <br/>
                 </section>
             </Container>
     </div>
