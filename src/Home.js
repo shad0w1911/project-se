c0e70d3cc0616341821d71f6fe9e3e639fab5c7e
Hassan Uddin Sheikh <unbundleracers@gmail.com>
2020-09-10 17:34:53 +0500

    carousel images updated

--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 import { Row, Col, Container } from "react-bootstrap";
+import image1 from './assets/home/01.jpg';
+import image2 from './assets/home/02.jpg';
+import image3 from './assets/home/03.jpg';
 
 export const Home = () => (
-  <div>
+        <div>
             <div>
                 <Carousel autoPlay>
                     <Carousel.Item>
                         <div className="img-gradient" >
-                            <img className="d-block w-100 carouselImage" alt="First slide" width="100%" src="https://zainenterprises.pk/wp-content/uploads/2018/08/background.jpg"/>
+                            <img className="d-block w-100 carouselImage" alt="First slide" width="100%" src={image1}/>
                         </div>
                         <Carousel.Caption className = "transparentBg">
                             <h3>First slide label</h3>
@@ -17,7 +20,7 @@ export const Home = () => (
                     </Carousel.Item>
                     <Carousel.Item>
                         <div className="img-gradient">
-                            <img className="d-block w-100 carouselImage" alt="Second slide" width="100%" src="https://constructionblog.autodesk.com/wp-content/uploads/2020/03/Construction-industry-statistics.jpg"/>
+                            <img className="d-block w-100 carouselImage" alt="Second slide" width="100%" src={image2}/>
                         </div>
                         <Carousel.Caption className = "transparentBg" >
                             <h3 > Second slide label </h3>
@@ -26,7 +29,7 @@ export const Home = () => (
                     </Carousel.Item>
                     <Carousel.Item>
                         <div className="img-gradient">
-                            <img className="d-block w-100 carouselImage" alt="Third slide" width="100%" src="https://images.pexels.com/photos/116832/pexels-photo-116832.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"/>
+                            <img className="d-block w-100 carouselImage" alt="Third slide" width="100%" src={image3}/>
                         </div>
                         <Carousel.Caption className="transparentBg">
                             <h3>Third slide label</h3>
@@ -40,17 +43,10 @@ export const Home = () => (
                     <div id = "aboutFD">
                         <Row >
                             <Col lg md>
-                                <h1 style={{
-    textAlign: "center"
-  }}>Sarwar Enterprises is a team of experts that strive for <span style={{
-    color: "#fb7840"
-  }}>perfection</span>.</h1>
+                                <h1 style={{textAlign: "center"}}>Sarwar Enterprises is a team of experts that strive for <span style={{color: "#fb7840"}}>perfection</span>.</h1>
                             </Col>
                             <Col lg md>
-                            <p style={{
-    textAlign: "center",
-    fontWeight: "100"
-  }}>
+                            <p style={{textAlign: "center", fontWeight: "100"}}>
                                 Sarwar Enterprises has a very efficient & professional setup with dedicated units of demolition, civil & plumbing works, wooden & metal works, electrical, lain & A/Cs works,
                                 aluminium, fire-fighting, medical-gases glass and all other related general works.
                             </p>
@@ -62,17 +58,10 @@ export const Home = () => (
                         <div>
                             <Row>
                                 <Col lg md>
-                                    <p style={{
-    fontSize: "40px",
-    fontWeight: "400",
-    color: "#fb7840"
-  }}>What we do</p>
+                                    <p style={{fontSize: "40px", fontWeight: "400", color: "#fb7840"}}>What we do</p>
                                 </Col>
                                 <Col lg md>
-                                    <p style={{
-    fontSize: "20px",
-    fontWeight: "300"
-  }}>
+                                    <p style={{fontSize: "20px",fontWeight: "300"}}>
                                         With a vast range of facilities, from the developments of basic concept to the high quality fabrication & construction work, electrical mechanical and plumbing, fire-fighting, medical-gases work.
                                         This firm is posied to fulfill any project all over the Pakistan.
                                     </p>
